80c95e158ba54e811eb5a96b62fbbd2badeb717e
Lishalj <lishajonwal@gmail.com>
2023-01-12 17:52:12 +0530

    lisha assessment

---
+++ script.js
@@ -0,0 +1,26 @@
+var login = document.getElementById("login")
+var signup = document.getElementById("signup")
+var loginMain = document.getElementById("login-main")
+var signMain = document.getElementById("sign-main")
+var main = document.getElementById("main")
+
+
+const loginfunc = () => {
+    login.style.borderBottomStyle="solid"
+    login.style.borderBottomWidth="6px"
+    login.style.borderBottomColor="#ff0066";
+    signup.style.borderBottomStyle="none"
+    loginMain.style.display="flex"
+    main.style.height="30%"
+    signMain.style.display="none"
+}
+const signupfunc = () => {
+    signup.style.borderBottomStyle="solid"
+    signup.style.borderBottomWidth="6px"
+    signup.style.borderBottomColor="#ff0066";
+    login.style.borderBottomStyle="none"
+    signMain.style.display="flex"
+    main.style.height="35%"
+    loginMain.style.display="none"
+
+}
